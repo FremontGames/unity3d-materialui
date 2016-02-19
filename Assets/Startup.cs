ec3d61a26e0e4c347230a9ffe4a42e0936ba9324
Damien FREMONT <damien.fremont@gmail.com>
2016-02-19 17:37:40 +0100

    added unity project

---
+++ Assets/Startup.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoad]
+public class Startup {
+	static Startup()
+	{
+		Debug.Log("Up and running");
+		Debug.Log("Linking...");
+		GameObject gameObject = GameObject.Find("Main Camera");
+		Debug.Log("gameObject:"+gameObject);
+		gameObject.AddComponent<Main2>();
+		Debug.Log("Linking done!");
+	}
+}
