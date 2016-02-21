3f24666fda1ee0ad5eb4987ff954622dc386d574
Damien FREMONT <damien.fremont@gmail.com>
2016-02-21 18:03:48 +0100

    removed unused logs

--- Assets/Startup.cs
+++ Assets/Startup.cs
@@ -7,7 +7,6 @@ public class Startup {
 	{
 		Debug.Log("----------------------------");
 		Debug.Log("EditorStartup:Up and running");
-		Debug.Log("EditorStartup:Linking...");
 		GameObject gameObject = GameObject.Find("Main Camera");
 		if (null == gameObject) {
 			Debug.Log("EditorStartup:gameObject 'Main Camera' must not be null!");
@@ -17,7 +16,5 @@ public class Startup {
 			Debug.Log("EditorStartup:setting Main script!");
 			gameObject.AddComponent<Main2> ();
 		}
-		// TODO if have component remove component
-		Debug.Log("EditorStartup:Linking done!");
 	}
 }
