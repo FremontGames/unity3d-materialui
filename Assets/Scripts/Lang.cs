b9c17e5ea90c7f3ef07fdededc5ce5e35ca8dc03
Damien FREMONT <damien.fremont@gmail.com>
2016-02-21 19:40:38 +0100

    added lang extension

---
+++ Assets/Scripts/Lang.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AssemblyCSharp
+{
+	public class Lang
+	{
+		public Lang ()
+		{
+		}
+
+		public void checkNotNull(Object arg) {
+			if(null == arg)
+				throw new FormatException();
+		}
+
+		public void checkNotEmpty(string arg) {
+			if(null == arg)
+				throw new FormatException();
+			if("" == arg)
+				throw new FormatException();
+			if(" " == arg)
+				throw new FormatException();
+		}
+			
+	}
+}
+
