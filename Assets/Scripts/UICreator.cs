d5e82ee84996e8d5e11116e98ee41923b790684a
Damien FREMONT <damien.fremont@gmail.com>
2016-03-01 13:43:30 +0100

    tested unity4 gui

--- Assets/Scripts/UICreator.cs
+++ Assets/Scripts/UICreator.cs
@@ -9,101 +9,179 @@ using UnityEngine.Events;
 /*
  * see http://chikkooos.blogspot.fr/2015/03/new-ui-implementation-using-c-scripts.html
  */
-// https://unity3d.com/learn/tutorials/topics/user-interface-ui
 public class UICreator : MonoBehaviour {
 
-	int fontSize;
-
-	void OnGUI() {
-
-		// STYLE
-		// http://docs.unity3d.com/Manual/gui-Customization.html
-		// http://docs.unity3d.com/Manual/class-GUIStyle.html
-		GUIStyle button_style = GUI.skin.GetStyle ("button");
-		button_style.fontSize = fontSize; 
-
-	/*
-		GUI.backgroundColor = Color.yellow;
-		GUI.Button(new Rect(10, 10, 70, 30), "A button");
-		*/
-
-		// http://docs.unity3d.com/ScriptReference/RectOffset-ctor.html
-		button_style.border = new RectOffset(1,1,10,30);
-		// NORMAL
-		// http://docs.unity3d.com/ScriptReference/Material-color.html
-		button_style.normal.textColor = Color.grey;
-		// http://docs.unity3d.com/ScriptReference/Resources.Load.html
-		button_style.normal.background = (Texture2D) Resources.Load("test.png", typeof(Texture2D));
-		// HOVER
-		button_style.onHover.textColor = Color.black;
-		// ACTIVE
-		button_style.active.textColor = Color.blue;
-		// button_style.active.background = (Texture2D) Resources.Load("test.png", typeof(Texture2D));
-
-		// UI
-		// http://docs.unity3d.com/ScriptReference/GUILayout.BeginArea.html
-		GUILayout.BeginHorizontal();
-		if (GUILayout.Button("Play"))
-	        play();
-		if (GUILayout.Button("Cancel"))
-	        cancel();
-			if (GUILayout.Button("Exit"))
-	        exit();
-        GUILayout.EndHorizontal();
-
-
-		// MIDDLE BAR
-		// http://docs.unity3d.com/ScriptReference/GUILayout.FlexibleSpace.html
-		GUILayout.BeginArea(new Rect(10, Screen.height/2, Screen.width-(10*2), 60));
-		GUILayout.BeginHorizontal();
-			GUILayout.Button("LEFT");
-			GUILayout.FlexibleSpace();
-			GUILayout.Button("RIGHT");
-			GUILayout.EndHorizontal();
-		GUILayout.EndArea();
-
-
-		// BOTTOM BAR
-		GUILayout.BeginArea(new Rect(10, 120, Screen.width-(10*2), 60));
-		GUILayout.BeginHorizontal();
-			GUILayout.Button("ACTION");
-		GUILayout.EndArea();
-
-	}
-
-	private void Update () {
-
-	}
-	
-	private void play()	{
-		Debug.Log("UI:play");
-	}
+	private const int LayerUI = 5;
 
 	void Start () {
-	//	CreateUI();
-
-		// http://docs.unity3d.com/ScriptReference/Application-platform.html
-		// OS
-		if (Application.platform == RuntimePlatform.WindowsPlayer) {
-			Debug.Log("setting for Desktop");
-			fontSize = Screen.width / 40;
-		}
-		// MOBILE
-		else {
-			Debug.Log("setting for Mobile");
-			fontSize = Screen.width / 15;
-		}
-
+		CreateUI();
 	}
 
-	private void exit()	{
-		Debug.Log("UI:exit");
+	void Update () {
+	}
+
+
+	private void CreateUI() {
+
+		GameObject canvas = CreateCanvas(this.transform);
+
+		CreateEventSystem(canvas.transform);
+
+		GameObject panel = CreatePanel(canvas.transform);
+
+		CreateText(panel.transform, 0, 100, 160, 50, "Message", 32);
+		CreateText(panel.transform, 0, 0, 160, 50, "Are you sure, you want to exit?", 24);
+
+		CreateButton(panel.transform, -100, -100, 160, 50, "Yes", delegate {OnExit();});
+		CreateButton(panel.transform, 100, -100, 160, 50, "No", delegate {OnCancel();});
+	}
+
+	private GameObject CreateCanvas(Transform parent) {
+		// create the canvas
+		GameObject canvasObject = new GameObject("Canvas");
+		canvasObject.layer = LayerUI;
+
+		RectTransform canvasTrans = canvasObject.AddComponent<RectTransform>();
+
+		Canvas canvas = canvasObject.AddComponent<Canvas>();
+		canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+		canvas.pixelPerfect = true;
+
+		CanvasScaler canvasScal = canvasObject.AddComponent<CanvasScaler>();
+		canvasScal.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+		canvasScal.referenceResolution = new Vector2(800, 600);
+
+		GraphicRaycaster canvasRayc = canvasObject.AddComponent<GraphicRaycaster>();
+
+		canvasObject.transform.SetParent(parent);
+
+		return canvasObject;
+	}
+
+	private GameObject CreateEventSystem(Transform parent) {
+		GameObject esObject = new GameObject("EventSystem");
+
+		EventSystem esClass = esObject.AddComponent<EventSystem>();
+		esClass.sendNavigationEvents = true;
+		esClass.pixelDragThreshold = 5;
+
+		StandaloneInputModule stdInput = esObject.AddComponent<StandaloneInputModule>();
+		stdInput.horizontalAxis = "Horizontal";
+		stdInput.verticalAxis = "Vertical";
+
+		TouchInputModule touchInput = esObject.AddComponent<TouchInputModule>();
+
+		esObject.transform.SetParent(parent);
+
+		return esObject;
+	}
+
+	private GameObject CreatePanel(Transform parent) {
+		GameObject panelObject = new GameObject("Panel");
+		panelObject.transform.SetParent(parent);
+
+		panelObject.layer = LayerUI;
+
+		RectTransform trans = panelObject.AddComponent<RectTransform>();
+		trans.anchorMin = new Vector2(0, 0);
+		trans.anchorMax = new Vector2(1, 1);
+		trans.anchoredPosition3D = new Vector3(0, 0, 0);
+		trans.anchoredPosition = new Vector2(0, 0);
+		trans.offsetMin = new Vector2(0, 0);
+		trans.offsetMax = new Vector2(0, 0);
+		trans.localPosition = new Vector3(0, 0, 0);
+		trans.sizeDelta = new Vector2(0, 0);
+		trans.localScale = new Vector3(0.8f, 0.8f, 1.0f);
+
+		CanvasRenderer renderer = panelObject.AddComponent<CanvasRenderer>();
+
+		Image image = panelObject.AddComponent<Image>();
+		Texture2D tex = Resources.Load<Texture2D>("panel_bkg");
+		image.sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height),
+			new Vector2(0.5f, 0.5f));
+
+		return panelObject;
+	}
+
+	private GameObject CreateText(Transform parent, float x, float y,
+		float w, float h, string message, int fontSize) {
+		GameObject textObject = new GameObject("Text");
+		textObject.transform.SetParent(parent);
+
+		textObject.layer = LayerUI;
+
+		RectTransform trans = textObject.AddComponent<RectTransform>();
+		trans.sizeDelta.Set(w, h);
+		trans.anchoredPosition3D = new Vector3(0, 0, 0);
+		trans.anchoredPosition = new Vector2(x, y);
+		trans.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+		trans.localPosition.Set(0, 0, 0);
+
+		CanvasRenderer renderer = textObject.AddComponent<CanvasRenderer>();
+
+		Text text = textObject.AddComponent<Text>();
+		text.supportRichText = true;
+		text.text = message;
+		text.fontSize = fontSize;
+		text.font = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font;
+		text.alignment = TextAnchor.MiddleCenter;
+		text.horizontalOverflow = HorizontalWrapMode.Overflow;
+		text.color = new Color(0, 0, 1);
+
+		return textObject;
+	}
+
+	private GameObject CreateButton(Transform parent, float x, float y,
+		float w, float h, string message,
+		UnityAction eventListner) {
+		GameObject buttonObject = new GameObject("Button");
+		buttonObject.transform.SetParent(parent);
+
+		buttonObject.layer = LayerUI;
+
+		RectTransform trans = buttonObject.AddComponent<RectTransform>();
+		SetSize(trans, new Vector2(w, h));
+		trans.anchoredPosition3D = new Vector3(0, 0, 0);
+		trans.anchoredPosition = new Vector2(x, y);
+		trans.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+		trans.localPosition.Set(0, 0, 0);
+
+		CanvasRenderer renderer = buttonObject.AddComponent<CanvasRenderer>();
+
+		Image image = buttonObject.AddComponent<Image>();
+
+		Texture2D tex = Resources.Load<Texture2D>("button_bkg");
+		image.sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height),
+			new Vector2(0.5f, 0.5f));
+
+		Button button = buttonObject.AddComponent<Button>();
+		button.interactable = true;
+		button.onClick.AddListener(eventListner);
+
+		GameObject textObject = CreateText(buttonObject.transform, 0, 0, 0, 0,
+			message, 24);
+
+		return buttonObject;
+	}
+
+	private static void SetSize(RectTransform trans, Vector2 size) {
+		Vector2 currSize = trans.rect.size;
+		Vector2 sizeDiff = size - currSize;
+		trans.offsetMin = trans.offsetMin -
+			new Vector2(sizeDiff.x * trans.pivot.x,
+				sizeDiff.y * trans.pivot.y);
+		trans.offsetMax = trans.offsetMax + 
+			new Vector2(sizeDiff.x * (1.0f - trans.pivot.x),
+				sizeDiff.y * (1.0f - trans.pivot.y));
+	}
+
+	private void OnExit()
+	{
 		Application.Quit();
 	}
 
-	private void cancel()	{
-		Debug.Log("UI:cancel");
+	private void OnCancel()
+	{
 		GameObject.Destroy(this);
 	}
-
 }
---
+++ Assets/Scripts/UICreator2.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+/*
+ * see http://chikkooos.blogspot.fr/2015/03/new-ui-implementation-using-c-scripts.html
+ */
+// https://unity3d.com/learn/tutorials/topics/user-interface-ui
+public class UICreator2 : MonoBehaviour {
+
+	int fontSize;
+
+	void OnGUI() {
+
+		// STYLE
+		// http://docs.unity3d.com/Manual/gui-Customization.html
+		// http://docs.unity3d.com/Manual/class-GUIStyle.html
+		GUIStyle button_style = GUI.skin.GetStyle ("button");
+		button_style.fontSize = fontSize; 
+
+		/*
+		GUI.backgroundColor = Color.yellow;
+		GUI.Button(new Rect(10, 10, 70, 30), "A button");
+		*/
+
+		// http://docs.unity3d.com/ScriptReference/RectOffset-ctor.html
+		button_style.border = new RectOffset(1,1,10,30);
+		// NORMAL
+		// http://docs.unity3d.com/ScriptReference/Material-color.html
+		button_style.normal.textColor = Color.grey;
+		// http://docs.unity3d.com/ScriptReference/Resources.Load.html
+		button_style.normal.background = (Texture2D) Resources.Load("test.png", typeof(Texture2D));
+		// HOVER
+		button_style.onHover.textColor = Color.black;
+		// ACTIVE
+		button_style.active.textColor = Color.blue;
+		// button_style.active.background = (Texture2D) Resources.Load("test.png", typeof(Texture2D));
+
+		// UI
+		// http://docs.unity3d.com/ScriptReference/GUILayout.BeginArea.html
+		GUILayout.BeginHorizontal();
+		if (GUILayout.Button("Play"))
+			play();
+		if (GUILayout.Button("Cancel"))
+			cancel();
+		if (GUILayout.Button("Exit"))
+			exit();
+		GUILayout.EndHorizontal();
+
+
+		// MIDDLE BAR
+		// http://docs.unity3d.com/ScriptReference/GUILayout.FlexibleSpace.html
+		GUILayout.BeginArea(new Rect(10, Screen.height/2, Screen.width-(10*2), 60));
+		GUILayout.BeginHorizontal();
+		GUILayout.Button("LEFT");
+		GUILayout.FlexibleSpace();
+		GUILayout.Button("RIGHT");
+		GUILayout.EndHorizontal();
+		GUILayout.EndArea();
+
+
+		// BOTTOM BAR
+		GUILayout.BeginArea(new Rect(10, 120, Screen.width-(10*2), 60));
+		GUILayout.BeginHorizontal();
+		GUILayout.Button("ACTION");
+		GUILayout.EndArea();
+
+	}
+
+	private void Update () {
+
+	}
+
+	private void play()	{
+		Debug.Log("UI:play");
+	}
+
+	void Start () {
+		//	CreateUI();
+
+		// http://docs.unity3d.com/ScriptReference/Application-platform.html
+		// OS
+		if (Application.platform == RuntimePlatform.WindowsPlayer) {
+			Debug.Log("setting for Desktop");
+			fontSize = Screen.width / 40;
+		}
+		// MOBILE
+		else {
+			Debug.Log("setting for Mobile");
+			fontSize = Screen.width / 15;
+		}
+
+	}
+
+	private void exit()	{
+		Debug.Log("UI:exit");
+		Application.Quit();
+	}
+
+	private void cancel()	{
+		Debug.Log("UI:cancel");
+		GameObject.Destroy(this);
+	}
+
+}
