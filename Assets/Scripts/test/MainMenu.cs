aff7c235fd3f44622e225172af1aec3e3f550379
Damien FREMONT <damien.fremont@gmail.com>
2016-03-16 12:49:50 +0100

    testing unity3d

--- Assets/Scripts/test/MainMenu.cs
+++ Assets/Scripts/test/MainMenu.cs
@@ -10,10 +10,11 @@ public class MainMenu : MonoBehaviour
 {
 	UIFactory f = new UIFactory();
 
+	// VIEW ***********************************************
+
 	// Use this for initialization
 	void Start ()
 	{
-
 		GameObject canvas = f.CreateCanvas(this.transform);
 
 //		f.CreateEventSystem(canvas.transform);
@@ -29,23 +30,6 @@ public class MainMenu : MonoBehaviour
 		f.CreateButton(panel.transform, 0, -200, 160, 50, "Yes", delegate {action2();});
 //		f.CreateButton(panel.transform, 100, -100, 160, 50, "No", delegate {OnCancel();});
 
-
-		/*
-
-		UnityAction someListener = new UnityAction (reaction);
-		EventManager.StartListening ("test", someListener);
-
-
-		GameObject canvasObject = new GameObject("Canvas2");
-		f.initCanvas (canvasObject);
-
-//		GameObject canvasObject2 = f.initCanvas2(this);
-
-		f.initButton2 (canvasObject, "tet3", delegate { action(); });
-
-		// http://docs.unity3d.com/Manual/JSONSerialization.html
-		// myObject = JsonUtility.FromJson<MyClass>(json);
-		*/
 	}
 
 	// Update is called once per frame
@@ -54,11 +38,15 @@ public class MainMenu : MonoBehaviour
 	
 	}
 
+	// PRESENTER ***********************************************
+
+
 	private void action()
 	{
 		Debug.Log("Menu2:action...");
 		EventManager.TriggerEvent ("test1");
 	}
+
 	private void action2()
 	{
 		Debug.Log("Menu2:action2...");
--- Assets/Scripts/test/TestsMenu.cs
+++ Assets/Scripts/test/TestsMenu.cs
@@ -8,70 +8,35 @@ using UnityEngine.Events;
 
 public class TestsMenu : MonoBehaviour
 {
-	UIFactory f = new UIFactory();
-
 	// Use this for initialization
 	void Start ()
 	{
+		// VIEW
+		UIFactory f = new UIFactory ();
+		// f.CreateEventSystem(canvas.transform);
+		GameObject c = f.CreateCanvas (this.transform);
+		GameObject p = f.CreatePanel (c.transform);
+		f.CreateText (p.transform, 0, 100, 160, 50, "Tests", 32);
+		f.CreateText (p.transform, 0, 0, 160, 50, "Choose a demo", 24);
+		f.CreateButton (p.transform, 0, -100, 300, 70, "Main", delegate {
+			EventManager.TriggerEvent ("open:MainMenu");
+		});
+		f.CreateButton (p.transform, 0, -200, 300, 70, "Editor", delegate {
+			EventManager.TriggerEvent ("open:EditorTest");
+		});
+		f.CreateButton (p.transform, 0, -300, 300, 70, "Map", delegate {
+			EventManager.TriggerEvent ("exec:MapMenu");
+		});
 
-		GameObject canvas = f.CreateCanvas(this.transform);
-
-//		f.CreateEventSystem(canvas.transform);
-		EventManager.StartListening ("exec:EditorTest", reaction);
-		EventManager.StartListening ("exec", reaction);
-
-		GameObject panel = f.CreatePanel(canvas.transform);
-
-		// make builder pattern or json
-		f.CreateText(panel.transform, 0, 100, 160, 50, "Tests", 32);
-		f.CreateText(panel.transform, 0, 0, 160, 50, "Choose a demo", 24);
-
-		f.CreateButton(panel.transform, 0, -100, 300, 70, "Main", delegate {action();});
-		f.CreateButton(panel.transform, 0, -200, 300, 70, "Editor", delegate {action2();});
-		f.CreateButton(panel.transform, 0, -300, 300, 70, "Map", delegate {action2();});
-
-		/*
-
-		UnityAction someListener = new UnityAction (reaction);
-		EventManager.StartListening ("test", someListener);
-
-
-		GameObject canvasObject = new GameObject("Canvas2");
-		f.initCanvas (canvasObject);
-
-//		GameObject canvasObject2 = f.initCanvas2(this);
-
-		f.initButton2 (canvasObject, "tet3", delegate { action(); });
-
-		// http://docs.unity3d.com/Manual/JSONSerialization.html
-		// myObject = JsonUtility.FromJson<MyClass>(json);
-		*/
-	}
-
-	// Update is called once per frame
-	void Update ()
-	{
-	
-	}
-
-	private void action()
-	{
-		Debug.Log("Menu2:action...");
-		EventManager.TriggerEvent ("exec:MainMenu");
-	}
-	private void action2()
-	{
-		Debug.Log("Menu2:action2...");
-		EventManager.TriggerEvent ("exec:EditorTest");
-	}
-
-
-	private void reaction()
-	{
-		Debug.Log("Menu2:reaction...");
-		Destroy (this);
-		GameObject gameObject = GameObject.Find ("Main Camera");
-		gameObject.AddComponent<EditorTest> ();
+		// PRESENTER
+		EventManager.StartListening ("open:MainMenu", delegate {
+			Destroy (this);
+			GameObject.Find ("Main Camera").AddComponent<MainMenu> ();
+		});
+		EventManager.StartListening ("open:EditorTest", delegate {
+			Destroy (this);
+			GameObject.Find ("Main Camera").AddComponent<EditorTest> ();
+		});
 	}
 
 }
