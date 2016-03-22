5e7c2cef6e7b01156058c02b456611aa561cdc50
Damien FREMONT <damien.fremont@gmail.com>
2016-03-22 13:52:53 +0100

    testing

---
+++ Assets/Scripts/bui/BUI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BUI
+{
+	Transform _parent;
+
+	public BUI(params BUI[] childs){
+//		_childs = childs;
+	}
+
+	public BUI(Transform parent, params BUI[] childs){
+		_parent = parent;
+//		_childs = childs;
+	}
+
+}
+
