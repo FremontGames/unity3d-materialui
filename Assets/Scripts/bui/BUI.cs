e30807d1b63cea5dcf9e50e06c428698c50f5455
Damien FREMONT <damien.fremont@gmail.com>
2016-03-22 13:54:34 +0100

    test

--- Assets/Scripts/bui/BUI.cs
+++ Assets/Scripts/bui/BUI.cs
@@ -1,15 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/*
+ * inspired by
+ * http://getbootstrap.com
+ */
 public class BUI
 {
 	Transform _parent;
 
-	public BUI(params BUI[] childs){
+	public BUI (params BUI[] childs)
+	{
 //		_childs = childs;
 	}
 
-	public BUI(Transform parent, params BUI[] childs){
+	public BUI (Transform parent, params BUI[] childs)
+	{
 		_parent = parent;
 //		_childs = childs;
 	}
--- Assets/Scripts/bui/BUI_Modal.cs
+++ Assets/Scripts/bui/BUI_Modal.cs
@@ -1,6 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/*
+ * inspired by
+ * http://getbootstrap.com/javascript/#modals
+ */
 public class BUI_Modal : BUI
 {
 
