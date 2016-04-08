787a2ac8996d3a644bac3e71cf0eefa143346470
Damien FREMONT <damien.fremont@gmail.com>
2016-04-08 13:57:17 +0200

    test

--- Assets/Startup.cs
+++ Assets/Startup.cs
@@ -1,15 +1,17 @@
 ﻿// TODO exclude from build
 using UnityEngine;
-/*
+
 using UnityEditor;
 
+// http://docs.unity3d.com/Manual/RunningEditorCodeOnLaunch.html
 [InitializeOnLoad]
-*/
 public class Startup {
 	static Startup()
 	{
-/*		Debug.Log("----------------------------");
+		Debug.Log("----------------------------");
 		Debug.Log("EditorStartup:Up and running");
+		EditorApplication.update += Update;
+/*
 		GameObject gameObject = GameObject.Find("Main Camera");
 		if (null == gameObject) {
 			Debug.Log("EditorStartup:gameObject 'Main Camera' must not be null!");
@@ -21,4 +23,22 @@ public class Startup {
 		}
 */
 	}
+
+	static void Update ()
+	{
+		Debug.Log("Updating");
+		GameObject obj = GameObject.Find ("menu");
+		if (null != obj) {
+			Debug.Log("menu");
+			// INJECT
+			MDInjecter.preview (obj.transform);
+			// START
+			SGUIButton[] s = obj.GetComponents<SGUIButton>();
+			for (int i = 0; i < s.Length; i++) {
+				Debug.Log("s"+s);
+				s [i].Start();
+			}
+		}
+	}
 }
+
