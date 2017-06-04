5e21fc57a464675a26f48d0bfbf1799de589d59b
Damien Frémont <damien.fremont@gmail.com>
2017-06-04 13:16:41 +0200

    feat(text) : police, font, style, color

---
+++ Assets/Scripts/components/MDText.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+namespace MDUI
+{
+    public class MDText : MonoBehaviour
+    {
+        // fields visible in Unity3d inspector
+
+
+        // Use this for initialization
+        void Start()
+        {
+            apply(this.GetComponentInChildren<RectTransform>());
+            apply(this.GetComponentInChildren<Text>());
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        // Use this for editor reset component button
+        void Reset()
+        {
+            apply(this.GetComponentInChildren<RectTransform>());
+            apply(this.GetComponentInChildren<Text>());
+        }
+
+        public static void apply(RectTransform comp)
+        {
+            SetSize(comp, new Vector2(160, 30));
+        }
+
+        [System.Obsolete]
+        public static void SetSize(RectTransform trans, Vector2 size)
+        {
+            Vector2 currSize = trans.rect.size;
+            Vector2 sizeDiff = size - currSize;
+            trans.offsetMin = trans.offsetMin -
+            new Vector2(sizeDiff.x * trans.pivot.x,
+                sizeDiff.y * trans.pivot.y);
+            trans.offsetMax = trans.offsetMax +
+            new Vector2(sizeDiff.x * (1.0f - trans.pivot.x),
+                sizeDiff.y * (1.0f - trans.pivot.y));
+        }
+        public static void apply(Text txt)
+        {
+            if (txt != null)
+            {
+                txt.font = MDTheme.get().font;
+                txt.fontSize = MDTheme.get().fontSize;
+                txt.color = MDTheme.get().color;
+                txt.fontStyle = MDTheme.get().fontStyle;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/components/MDTheme.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace MDUI
+{
+    public class MDTheme
+    {
+        private static MDTheme singleton = null;
+        public static MDTheme get()
+        {
+            if (singleton == null)
+                singleton = new MDTheme();
+            return singleton;
+        }
+
+        public Font font;
+        public int fontSize;
+        public Color color;
+        public FontStyle fontStyle;
+
+        MDTheme()
+        {
+            font = Resources.Load<Font>("Fonts/Roboto-Regular");
+            fontSize = 14;
+            fontStyle = FontStyle.Normal;
+            color = new Color(0, 0, 0, 0.87f);
+        }
+
+    }
+}
