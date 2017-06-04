e2e01f7e79889086f01b66b3777f6da597499196
Damien Frémont <damien.fremont@gmail.com>
2017-06-04 23:30:29 +0200

    refactoring: remove deprecated api

---
+++ Assets/Scripts/MDButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+namespace MDUI
+{
+    public class MDButton : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/MDCard.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+namespace MDUI
+{
+    public class MDContent : MonoBehaviour
+    {
+        public bool layoutPadding = true;
+
+        // Use this for initialization
+        void Start()
+        {
+            // apply(this.GetComponentInChildren<MDCardTitle>());
+            // apply(this.GetComponentInChildren<MDCardSubTitle>());
+            // apply(this.GetComponentInChildren<MDCardContent>());
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
