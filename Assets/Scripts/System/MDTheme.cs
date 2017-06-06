7800c90bc0b591d78c395ab1835626edc273114d
Damien Frémont <damien.fremont@gmail.com>
2017-06-06 11:24:39 +0200

    feat(style): typo

---
+++ Assets/Scripts/System/MDTheme.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace MDUI
+{
+    public class MDTheme
+    {
+        // fields visible in Unity3d inspector
+        public string primaryPalette = "indigo";
+        public string accentPalette = "pink";
+        public string warnPalette = "red";
+        public string backgroundPalette = "grey";
+
+        public Color color = new Color(0, 0, 0, 0.87f);
+
+        private static MDTheme singleton = null;
+        public static MDTheme get()
+        {
+            if (singleton == null)
+                singleton = new MDTheme();
+            return singleton;
+        }
+
+    }
+}
