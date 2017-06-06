a73697c206ccb6fa935c65fd62872f36ed09bf93
Damien Frémont <damien.fremont@gmail.com>
2017-06-06 19:07:04 +0200

    doc(demo)

---
+++ Assets/LoadOnClick.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MDUI
+{
+    // https://docs.unity3d.com/ScriptReference/SceneManagement.SceneManager.LoadScene.html
+
+    public class LoadOnClick : MonoBehaviour
+    {
+
+        public void LoadScene(string scene)
+        {
+            Debug.Log("scene2 loading: " + scene);
+            SceneManager.LoadScene(scene, LoadSceneMode.Single);
+        }
+    }
+}
