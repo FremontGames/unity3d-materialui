c7205e8cc10849be28e719c598d0f980c0f1c889
Damien Frémont <damien.fremont@gmail.com>
2017-06-06 18:38:23 +0200

    refactoring: move to package

---
+++ Assets/Scripts/MDLayout.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace MDUI
+{
+    // TODO https://docs.unity3d.com/ScriptReference/Screen-dpi.html
+    // TODO fix ratio
+    public class MDLayout : MonoBehaviour
+    {
+
+        // fields visible in Unity3d inspector
+        public MDLayoutType layout = MDLayoutType.Column;
+        public MDLayoutAlign layoutAlign = MDLayoutAlign.CenterCenter;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        // Use this for editor reset component button
+        void Reset()
+        {
+            Start();
+        }
+
+    }
+
+    public enum MDLayoutType
+    {
+        Row, Column
+    }
+
+    public enum MDLayoutAlign
+    {
+        StartStart,
+        StartCenter,
+        StartEnd,
+        StartStretch,
+
+        CenterStart,
+        CenterCenter,
+        CenterEnd,
+        CenterStretch,
+
+        EndStart,
+        EndCenter,
+        EndEnd,
+        EndStretch,
+
+        SpaceAroundStart,
+        SpaceAroundCenter,
+        SpaceAroundEnd,
+        SpaceAroundStretch,
+
+        SpaceBetweenStart,
+        SpaceBetweenCenter,
+        SpaceBetweenEnd,
+        SpaceBetweenStretch,
+    }
+
+}
